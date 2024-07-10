d439c746792316cf771b49fe8bed06711325b2db
Eduardo <146763563+eduardo-mw@users.noreply.github.com>
2024-07-10 16:21:39 -0700

    base setup

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).json({ message: "Express and TypeScript server!" });
+  });
+
+app.get("/health", (req: Request, res: Response) => {
+  res.status(200).json({ status: "up" });
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
